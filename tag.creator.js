0d936276a89ad348b1e9c1f83e42b4492e310449
Dmitry Vapelnik <dvapelnik@gmail.com>
2013-05-05 11:55:41 +0300

    Added docs, info and usages

--- tag.creator.js
+++ tag.creator.js
@@ -1,3 +1,16 @@
+/**
+ * Author Dmitry Vapelnik (dvapelnik@gmail.com)
+ * 04.04.2013
+ *
+ * usages:
+ * console.log(createTag('div', {id: "some id", style:{margin:"10px", display: "inline-block"}}, 'text'));
+ * console.log(createTag('img', {src: "/some/image.jpg", style: {width: "10px", height: "6px"}, class: "some-class"}))
+ *
+ * @param tag Tag name
+ * @param attributes Attributes as object with fields (style is object also)
+ * @param filling Filling of tag. Can be createTg also
+ * @returns {string} Constructed HTML tag with attributes and filling as string
+ */
 function createTag(tag, attributes, filling) {
     var html = '<' + tag;
     for (var propName in attributes) {
